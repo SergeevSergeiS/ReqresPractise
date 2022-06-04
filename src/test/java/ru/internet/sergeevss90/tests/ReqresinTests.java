4269c5c795dbe99d226bacb57f2074e04b80e4c3
SergeevSS90 <sergeevss90@internet.ru>
2022-06-04 10:18:32 +0400

    Added register tests

---
+++ src/test/java/ru/internet/sergeevss90/tests/ReqresinTests.java
@@ -0,0 +1,83 @@
+package ru.internet.sergeevss90.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresinTests {
+    @Test
+    void registerTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4));
+    }
+
+    @Test
+    void missingPasswordRegisterTest() {
+        String body = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void loginTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void missingPasswordLoginTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
